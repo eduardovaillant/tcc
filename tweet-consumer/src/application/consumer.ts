1701cd1a1a46b4a093aadd3d6f18e9e2c481d4cb
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-09-29 20:49:07 -0300

    feat: add dev and prod environment variables

--- tweet-consumer/src/application/consumer.ts
+++ tweet-consumer/src/application/consumer.ts
@@ -3,6 +3,6 @@ import { MessageBrokerService } from '@/infra/services'
 import env from '@/config/env'
 
 export const consumerFactory = async (brokerService: MessageBrokerService): Promise<void> => {
-  await MongoHelper.connect(env.mongoUrl)
+  await MongoHelper.connect(env.environment === 'dev' ? env.mongoUrlDev : env.mongoUrlProd)
   await brokerService.consume()
 }
--- tweet-consumer/src/config/env.ts
+++ tweet-consumer/src/config/env.ts
@@ -2,8 +2,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/TCC',
+  mongoUrlDev: process.env.MONGO_DEV_URL || 'mongodb://localhost:27017/TCC',
+  mongoUrlProd: process.env.MONGO_PROD_URL || '',
   rabbitmqUrl: process.env.RABBITMQ_URL || 'amqp://localhost',
   mongoCollection: process.env.MONGO_COLLECTION || 'tweets',
-  queueName: process.env.QUEUE_NAME || 'tweets'
+  queueName: process.env.QUEUE_NAME || 'tweets',
+  environment: process.env.ENVIRONMENT || 'dev'
 }

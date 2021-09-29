7c127304801db1f9cf2f8f36c0f8edb24b1ec904
Eduardo Brito Vaillant <eduardo.bvaillant@gmail.com>
2021-09-29 15:32:53 -0300

    refactor: update tweet consumer

--- tweet-producer/src/infra/services/message-broker-service.ts
+++ tweet-producer/src/infra/services/message-broker-service.ts
@@ -4,7 +4,6 @@ import { TweetRepository } from '@/infra/db'
 import amqp from 'amqplib'
 
 export class MessageBrokerService {
-  private readonly queue: string = 'tweets'
   private channel: any
   private connection: any
 
@@ -15,15 +14,11 @@ export class MessageBrokerService {
   async createConnection (): Promise<void> {
     this.connection = await amqp.connect(env.rabbitmqUrl)
     this.channel = await this.connection.createChannel()
-    await this.channel.assertQueue(this.queue, { durable: true })
-  }
-
-  publish (msg: string): void {
-    this.channel.sendToQueue(this.queue, Buffer.from(msg))
+    await this.channel.assertQueue(env.queueName, { durable: true })
   }
 
   async consume (): Promise<void> {
-    await this.channel.consume(this.queue, async (msg) => {
+    await this.channel.consume(env.queueName, async (msg) => {
       if (msg) {
         const tweet = JSON.parse(msg.content.toString())
         await this.tweetRepository.insertOne(tweet)
